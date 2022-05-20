7a05ec39ffa48039aac36f1ec587dca9d059a9a9
LuiUribe <105945383+LuiUribe@users.noreply.github.com>
2022-05-20 15:46:19 -0400

    Update player.py

--- player.py
+++ player.py
@@ -8,6 +8,7 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect(topleft = pos)
         
         self.direction = pygame.math.Vector2()
+        self.speed = 5
         
     def input(self):
         keys = pygame.key.get_press
@@ -26,5 +27,12 @@ class Player(pygame.sprite.Sprite):
         else:
             self.direction = 0
             
+    def move(self,speed):
+        if.self.direction.magnitude() != 0:
+            self.direction = self.direction.normalzie()
+            
+        self.rect.center += self.direction * speed
+            
     def update(self):
         self.input()
+        self.move(self.speed)
