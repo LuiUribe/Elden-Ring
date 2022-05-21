c58e09369741aa959a00bd590287d049d5007cdd
LuiUribe <105945383+LuiUribe@users.noreply.github.com>
2022-05-21 13:12:47 -0400

    Update player.py

--- player.py
+++ player.py
@@ -31,7 +31,6 @@ class Player(pygame.sprite.Sprite):
         for animation in self.animations.keys():
             full_path = character_path + animation
             self.animations[animation] = import_folder(full_path)
-        print(self.animations)
          
     def input(self):
         keys = pygame.key.get_pressed()
@@ -39,15 +38,19 @@ class Player(pygame.sprite.Sprite):
         # movement input
         if keys[pygame.K_UP]:
             self.direction.y = -1
+            self.status = 'up'
         elif keys[pygame.K_DOWN]:
             self.direction.y = 1
+            self.status = 'down'
         else:
             self.direction = 0
             
         if keys[pygame.K_RIGHT]:
             self.direction.x = 1
+            self.status = 'right'
         elif keys[pygame.K_LEFT]:
             self.direction.x = -1
+            self.status = 'left'
         else:
             self.direction = 0
             
@@ -67,7 +70,20 @@ class Player(pygame.sprite.Sprite):
         
         # idle status
         if self.direction.x == 0 and self.direction.y == 0:
-            self.status = self.status + '_idle'
+            if not 'idle' in self.status and not 'attack' in self.status:
+                self.status = self.status + '_idle'
+                
+        if self.attacking:
+            self.direction.x = 0
+            self.direction.y = 0
+            if not 'attack' in self.status:
+                if 'idle' in self.status:
+                    self.status = self.status.replace('_idle','_attack')
+                else:
+                    self.status = self.status + '_attack'
+         else : 
+              if 'attack' in self.status:
+                  self.status = self.status.replace('_attack','')
             
     def move(self,speed):
         if.self.direction.magnitude() != 0:
@@ -106,5 +122,5 @@ class Player(pygame.sprite.Sprite):
     def update(self):
         self.input()
         self.cooldowns()
-        self.get_status()
+        # self.get_status()
         self.move(self.speed)
