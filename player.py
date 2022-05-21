0c1fe495ec02b3e94308e9d434ef21659a6b3225
LuiUribe <105945383+LuiUribe@users.noreply.github.com>
2022-05-21 10:24:59 -0400

    Update player.py

--- player.py
+++ player.py
@@ -33,8 +33,29 @@ class Player(pygame.sprite.Sprite):
         if.self.direction.magnitude() != 0:
             self.direction = self.direction.normalzie()
             
-        self.rect.center += self.direction * speed
-            
+       self.rect.x += self.rection.x * speed
+       self.collision('horizontal')
+       self.rect.y += self.rection.y * speed   
+       self.collision('vertical')
+       # self.rect.center += self.direction * speed
+       
+    def collision(self,direction):
+        if direction == 'horizontal':
+            for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.x > 0: #moving right
+                        self.rect.right = sprite.rect.left
+                    if self.direction.x < 0: #moving left
+                        self.rect.left = sprite.rect.right
+                   
+        if direction == 'vertical':
+             for sprite in self.obstacle_sprites:
+                if sprite.rect.colliderect(self.rect):
+                    if self.direction.y > 0: #moving down
+                        self.rect.bottom = sprite.rect.top
+                    if self.direction.y < 0: #moving up
+                        self.rect.top = sprite.rect.bottom
+    
     def update(self):
         self.input()
         self.move(self.speed)
