da6a6a25650d70506a9af7951cca0a6fd195dd63
LuiUribe <105945383+LuiUribe@users.noreply.github.com>
2022-05-21 18:47:43 -0400

    Update ui.py

--- ui.py
+++ ui.py
@@ -3,4 +3,10 @@ from settings import *
 
 class UI:
   def __init__(Self):
+      
+      # general info
+      self.display_surface = pygame.display.get_surface()
+      self.font
+
+  def display(self,player):
       pass
