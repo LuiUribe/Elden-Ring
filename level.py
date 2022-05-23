1c40b6fb835153babbc84e61a223ee250a2c94e1
LuiUribe <105945383+LuiUribe@users.noreply.github.com>
2022-05-23 08:46:18 -0400

    Update level.py

--- level.py
+++ level.py
@@ -8,6 +8,7 @@ from random import choice
 from weapon import Weapon
 from ui import UI
 from enemy import Enemy
+from particles import Animation Player
 
 class Level:
     def __init__(self):
@@ -29,6 +30,9 @@ class Level:
         
         # user interface
         self.ui = UI()
+        
+        # particles in particles py
+        self.animation_player = AnimationPlayer()
         
     def create_map(self):
         layouts = {
@@ -102,7 +106,7 @@ class Level:
                 collision_sprites = pygame.sprite.spritecollide(attack_sprite,self.attackable_sprites,False)
                 if collision_sprites:
                     for target_sprite in collision_sprites:
-                        if target_sprite.sprite_type == 'grass':
+                        if target_sprite.sprite_type == 'grass'
                             target_sprite.kill()
                         else:
                             target_sprite.get_damage(self.player,attack_sprite.sprite_type)
