eb93ad9b4ee9e2c866af9176ad757125ebb6f9da
LuiUribe <105945383+LuiUribe@users.noreply.github.com>
2022-05-23 12:07:50 -0400

    Update magic.py

--- magic.py
+++ magic.py
@@ -5,7 +5,7 @@ class MagicPlayer:
     def __init__(self,animation_player):
         self.animation_player = animation_player
         
-    def heal(self):
+    def heal(self,player,strength,cost,groups):
         print('heal')
       
     def flame(self):
