bc291c6f5eaf0e9647ecbfb1a8201b4c6a6a730f
LuiUribe <105945383+LuiUribe@users.noreply.github.com>
2022-05-23 13:02:37 -0400

    Update level.py

--- level.py
+++ level.py
@@ -16,6 +16,7 @@ class Level:
         
         # get the display surface
         self.display_surface = pygame.display.get_surface()
+        self.game_paused = False
         
         # sprite group setup
         self.visible_sprites = YSortCameraGroup()
@@ -136,14 +137,22 @@ class Level:
     def add_exp(self,amount):
         
         self.player.exp += amount
+        
+    def toggle_menu(self):
+        
+        self.game_paused = not self.game_paused
        
     def run(self):
-        # update and draw the game
         self.visible_sprites.custom_draw(self.player)
-        self.visible_sprites.update()
-        self.visible_sprites.enemy_update(self.player)
-        self.player_attack_logic()
-        self.ui.display(self.player)
+                self.ui.display(self.player)
+        
+        if game_paused:
+            pass
+            # display upgrade menu
+        else:
+            self.visible_sprites.update()
+            self.visible_sprites.enemy_update(self.player)
+            self.player_attack_logic()
         
         
 class YSortCameraGroup(pygame.sprite.Group):
