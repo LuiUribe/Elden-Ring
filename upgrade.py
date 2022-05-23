5858a3dcd14331fc8bcda557f084b8dc385c62ce
LuiUribe <105945383+LuiUribe@users.noreply.github.com>
2022-05-23 13:15:11 -0400

    Update upgrade.py

--- upgrade.py
+++ upgrade.py
@@ -7,6 +7,20 @@ class Upgrade:
       # general setup
       self.display_surface = pygame.display.get_surface()
       self.player = player
+      self.attribute_nr = len(player.stats)
+      self.attribute_names = list(player.stats.keys())
+      self.font = pygame.font.Font('UI_FONT, UI_FONT_SIZE')
+      
+  def input(self):
+      keys = pygame.key.get_pressed()
+      
+      if keys[pygame.K_RIGHT]:
+        pass
+      elif keys[pygame.K_LEFT]:
+        pass
+      
+      if keys[pygame.K_SPACE]:
+        pass
       
   def display(self):
       self.display_surface.fill('black')
